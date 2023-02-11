d18d370976b2a56846512e450a2ac5b637f5f5d0
sonya <sofiya.fedorova@mail.ru>
2023-02-11 18:23:07 +0300

    change names of classes

--- JQuiz/src/test/java/result/TestEqualityOfDifferentResultsWithSameFields.java
+++ JQuiz/src/test/java/result/TestEqualityOfDifferentResultsWithSameFields.java
@@ -7,9 +7,9 @@ import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 
-public class TestEqualityOfDifferentUsersWithSameFields {
+public class TestEqualityOfDifferentResultsWithSameFields {
     @Test
-    public void testEqualityOfDifferentUsersWithSameFields() {
+    public void testEqualityOfDifferentResultsWithSameFields() {
         Date date = new Date();
         Result result1 = new Result("name", date, 10);
         Result result2 = new Result("name", date, 10);
