26540e1f6f473727a2fea15fbdc5f94cb2ef60d1
Sabina <dzucevasabina@gmail.com>
2023-02-11 18:25:23 +0300

    Add tests with fixed after

--- JQuiz/src/test/java/user/TestFindExistingUser.java
+++ JQuiz/src/test/java/user/TestFindExistingUser.java
@@ -1,4 +1,35 @@
 package user;
 
+import core.DatabaseManager;
+import core.exceptions.UserAlreadyExistsException;
+import model.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
 public class TestFindExistingUser {
+    private DatabaseManager databaseManager;
+    private final User preAddedUser = new User("preAddedUser1", "1234");
+
+    @Before
+    public void before() throws IOException, UserAlreadyExistsException {
+        databaseManager = new DatabaseManager("db_test");
+        databaseManager.createDbDirectory();
+        databaseManager.addUser(preAddedUser);
+    }
+
+    @Test
+    public void findExistingUser() {
+        String requiredUsername = preAddedUser.getName();
+        assertTrue(databaseManager.isExistUser(requiredUsername));
+    }
+
+    @After
+    public void after() {
+        databaseManager.clearDb();
+    }
 }
